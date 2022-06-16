80db00f45b997f2cd5172da345dc80b88c783160
Esteban Becker <git@beckeresteban.eu>
2022-06-16 17:29:03 +0200

    Finishing

--- keystroke-recognition.py
+++ keystroke-recognition.py
@@ -22,9 +22,9 @@ with open('DSL-StrongPasswordData.csv') as csvfile:
 
                 subjects[row[0]] = data
             
-            subjects[row[0]]['KD'].append(row[3])
-            subjects[row[0]]['DDKL'].append(row[4])
-            subjects[row[0]]['UUKL'].append(row[3]+row[4])
+            subjects[row[0]]['KD'].append((float(row[3]),float(row[6]),float(row[9]),float(row[12]),float(row[15]),float(row[18]),float(row[21]),float(row[24]),float(row[27]),float(row[30]),float(row[33])))
+            subjects[row[0]]['DDKL'].append((float(row[4]),float(row[7]),float(row[10]),float(row[13]),float(row[16]),float(row[19]),float(row[22]),float(row[25]),float(row[28]),float(row[31]),))
+            subjects[row[0]]['UUKL'].append((float(row[3])+float(row[4]),float(row[6])+float(row[7]),float(row[9])+float(row[10]),float(row[12])+float(row[13]),float(row[15])+float(row[16]),float(row[18])+float(row[19]),float(row[21])+float(row[22]),float(row[24])+float(row[25]),float(row[27])+float(row[28]),float(row[30])+float(row[31])))
 
 train_data={}
 test_data={}
@@ -56,7 +56,44 @@ for subject in subjects:
             train_data[subject]['DDKL'].append(subjects[subject]['DDKL'][i])
             train_data[subject]['UUKL'].append(subjects[subject]['UUKL'][i])
 
-GMM=[]
-for subject in train_data:
-    GMM.append(GaussianMixture(n_components=3, covariance_type='full').fit((subject[1]['KD'],subject[1]['DDKL'],subject[1]['UUKL'])))
+GMM={}
 
+for subject in train_data:
+    GMM[subject] = {}
+    print("Training subject "+subject)
+    GMM[subject]['KD'] = GaussianMixture(n_components=2, covariance_type='full').fit(train_data[subject]['KD'])
+    GMM[subject]['DDKL'] = GaussianMixture(n_components=2, covariance_type='full').fit(train_data[subject]['DDKL'])
+    GMM[subject]['UUKL'] = GaussianMixture(n_components=2, covariance_type='full').fit(train_data[subject]['UUKL'])
+
+#Make prediction on the test data
+correct_prediction_KD=0
+correct_prediction_DDKL=0
+correct_prediction_UUKL=0
+
+for subject in test_data:
+    print("Testing subject "+subject)
+
+    prediction={}
+    prediction["KD"]={}
+    prediction["DDKL"]={}
+    prediction["UUKL"]={}
+
+    for gaussin in GMM:
+        prediction["KD"][gaussin] = GMM[gaussin]["KD"].score(test_data[subject]["KD"])
+        prediction["DDKL"][gaussin] = GMM[gaussin]["DDKL"].score(test_data[subject]["DDKL"])
+        prediction["UUKL"][gaussin] = GMM[gaussin]["UUKL"].score(test_data[subject]["UUKL"])
+
+    max_gaussian_KD=max(prediction["KD"].items(), key=lambda x: x[1])
+    max_gaussian_DDKL=max(prediction["DDKL"].items(), key=lambda x: x[1])
+    max_gaussian_UUKL=max(prediction["UUKL"].items(), key=lambda x: x[1])
+
+    if max_gaussian_KD[0] == subject:
+        correct_prediction_KD+=1
+    if max_gaussian_DDKL[0] == subject:
+        correct_prediction_DDKL+=1
+    if max_gaussian_UUKL[0] == subject:
+        correct_prediction_UUKL+=1
+
+print("KD: "+str(correct_prediction_KD/len(test_data)))
+print("DDKL: "+str(correct_prediction_DDKL/len(test_data)))
+print("UUKL: "+str(correct_prediction_UUKL/len(test_data)))
